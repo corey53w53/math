0c44204101188c7054d7f7bf29a45225ea90d280
Corey Wang <corefire2004@gmail.com>
2022-04-09 01:50:25 -0400

    multiply method

--- matrix.py
+++ matrix.py
@@ -15,6 +15,21 @@ class Matrix:
                 counter+=1
             big_list.append(small_list)
         self.values=big_list
+        list_rows=[]
+        for r in self.values:
+            small_list=[]
+            for c in r:
+                small_list.append(c)
+            list_rows.append(small_list)
+        self.list_rows=list_rows
+        list_cols=[]
+        
+        for c in range(self.col):
+            small_list=[]
+            for r in range(self.row):
+                small_list.append(list_rows[r][c])
+            list_cols.append(small_list)
+        self.list_cols=list_cols
     def __str__(self):
         big_s=""
         for small_list in self.values:
@@ -29,11 +44,17 @@ class Matrix:
     def __sub__(self,m2):
         assert (self.col==m2.col and self.row==m2.row),"columns or rows are not equal"
         return Matrix(self.row,self.col,[(self.raw_values[c]-m2.raw_values[c]) for c in range(len(self.raw_values))])
-    
-m1=Matrix(3,3,[100,2,3,4,5,600000,7,8,-100])
-m2=Matrix(3,3,[1,2,3,4,5,6,7,8,9])
-m1-=m2
-print(m1)
-x=100
-x-=1
-print(x)
+    def __mul__(self,m2):
+        assert (self.col==m2.row), "number of columns in first matrix does not equal number of rows in second matrix"
+        raw_values=[]
+        for row in self.list_rows:
+            for col in m2.list_cols:
+                raw_values.append(sum([row[c]*col[c] for c in range(self.col)]))
+        return Matrix(self.row,m2.col,raw_values)
+        
+# m1=Matrix(3,3,[2,2,3,4,5,100,7,8,-10])
+# m2=Matrix(3,3,[1,2,3,4,5,6,7,8,9])
+# print(m1*m2)
+m3=Matrix(2,3,[1,2,3,4,5,6])
+m4=Matrix(3,1,[7,8,9])
+print(m3*m4)
