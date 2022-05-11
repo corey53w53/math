2420a53755373ed1c6256e57f4efd05793ba2700
Corey Wang <corefire2004@gmail.com>
2022-05-11 16:33:23 -0400

    better names, final

--- discrete_6-6.py
+++ discrete_6-6.py
@@ -1,23 +1,24 @@
+#Corey Wang Discrete 6-6 Homework
 from random import *
 num_sims=10000000
-round_num_sum=0
+num_rounds_sum=0
 hanks_wins=0
 for _ in range(num_sims):
-    hchips=4
-    tchips=2
+    hanks_chips=4
+    teds_chips=2
     num_rounds=0
-    while hchips and tchips:
+    while hanks_chips and teds_chips:
         if randint(0,2):
-            hchips-=1
-            tchips+=1
+            hanks_chips-=1
+            teds_chips+=1
         else:
-            hchips+=1
-            tchips-=1
+            hanks_chips+=1
+            teds_chips-=1
         num_rounds+=1
-    if hchips:
+    if hanks_chips:
         hanks_wins+=1
-    round_num_sum+=num_rounds
-print(f'Average number of rounds in {num_sims} trials: {round_num_sum/num_sims}')
+    num_rounds_sum+=num_rounds
+print(f'Average number of rounds in {num_sims} trials: {num_rounds_sum/num_sims}')
 print(f'Hank\'s wins: {hanks_wins}')
 print(f'Ted\'s wins: {num_sims-hanks_wins}')
 
